9309954351406f505eb00ec2c4a4980e3d484ebb
Juan Carlos <jvalerio@reprintsdesk.com>
2023-09-26 12:17:02 -0600

    fix(App): :bug: Add lint exeption rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+/* eslint-disable react/react-in-jsx-scope */
 import './App.css';
 import { Button } from './components/ui/button';
 
 function App() {
   return (
-
     <div>
       <h1>Vite + React</h1>
       <Button>Click me</Button>
